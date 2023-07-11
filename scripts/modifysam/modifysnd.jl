e0bd6c09f7374d5cd0804a33924bf057c05c0116
Nathanael Wong <natgeo.wong@outlook.com>
2023-07-11 01:56:50 -0400

    Adding modifysnd to do sounding perturbations

---
+++ scripts/modifysam/modifysnd.jl
@@ -0,0 +1,21 @@
+using DrWatson
+@quickactivate "ExploreWTGSpace"
+using Printf
+
+include(srcdir("samsnd.jl"))
+include(srcdir("sam.jl"))
+
+expii = "P1282km300V64"
+
+z,p,_,q,t,_ = readsnd(projectdir("exp","snd",expii))
+nz = length(z)
+snddata = zeros(nz,6)
+snddata[:,1] .= z
+snddata[:,2] .= p
+snddata[:,4] .= q
+
+t_h = t .+ 0.05; snddata[:,3] .= t_h .* (1000 ./p).^(287/1004)
+createsndmean("$(expii)_hot",snddata;psfc=1009.32)
+
+t_h = t .- 0.05; snddata[:,3] .= t_h .* (1000 ./p).^(287/1004)
+createsndmean("$(expii)_cld",snddata;psfc=1009.32)
