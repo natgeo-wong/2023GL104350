c1cc30e4dee6072d9cd6764ad24854b2741346bf
Nathanael Wong <natgeo.wong@outlook.com>
2023-07-26 03:56:50 -0400

    Adding SP2 series of experimental runs

---
+++ scripts/modifysam/modifyprm_SP2.jl
@@ -0,0 +1,47 @@
+using DrWatson
+@quickactivate "2023GL104350"
+using Logging
+using Printf
+
+include(srcdir("sam.jl"))
+
+expname = "P1282km300V64"
+tprm  = projectdir("exp","tmp.prm")
+plist1 = [
+    0,sqrt(2),2,2*sqrt(2.5),5,5*sqrt(2),
+    10,10*sqrt(2),20,20*sqrt(2.5),50,50*sqrt(2),
+]
+plist2 = vcat(
+    plist1/100,1,
+    plist1
+)
+
+for powerii in plist1
+    conii = relaxscalestrprnt(powerii)
+    mkpath(projectdir("exp","prm","SP2",expname,conii))
+    for imember = 1 : 15
+        mstr = @sprintf("%02d",imember)
+        oprm = projectdir("run","modifysam","prmtemplates","SP2_$(expname).prm")
+        nprm = projectdir("exp","prm","SP2",expname,conii,"member$(mstr).prm")
+        open(tprm,"w") do fprm
+            open(oprm,"r") do rprm
+                s = read(rprm,String)
+                s = replace(s,"[xx]" => mstr)
+                s = replace(s,"[en]" => "$(imember)")
+                s = replace(s,"[am]" => @sprintf("%7e",1))
+                if !iszero(powerii)
+                    s = replace(s,"[bool]" => "true")
+                    s = replace(s,"[tau]" => @sprintf("%7e",powerii))
+                else
+                    s = replace(s,"[bool]" => "false")
+                    s = replace(s,"[tau]" => @sprintf("%7e",1))
+                end
+                s = replace(s,"e+" => "e")
+                write(fprm,s)
+            end
+        end
+        mkpath(projectdir("exp","prm","SP2",expname,conii))
+        mv(tprm,nprm,force=true)
+        @info "Creating new prm file for SP2 $expname $conii ensemble member $imember"
+    end
+end
---
+++ scripts/modifysam/modifyrun_SP2.jl
@@ -0,0 +1,68 @@
+using DrWatson
+@quickactivate "2023GL104350"
+using Printf
+
+include(srcdir("sam.jl"))
+
+expname = "P1282km300V64"
+pwrvec1 = [
+    0,sqrt(2),2,2*sqrt(2.5),5,5*sqrt(2),
+    10,10*sqrt(2),20,20*sqrt(2.5),50,50*sqrt(2),
+]
+pwrvec2 = vcat(plist1/100,1,plist1)
+pwrvec  = pwrvec1
+
+mrun = projectdir("run","modifysam","runtemplates","modelrun.sh")
+brun = projectdir("run","modifysam","runtemplates","Build.csh")
+
+open(mrun,"r") do frun
+    s = read(frun,String)
+    for pwrii in pwrvec
+
+        pwrname = relaxscalestrprnt(pwrii)
+
+        for ensembleii in 1 : 15
+
+            mstr = @sprintf("%02d",ensembleii)
+            nrun = projectdir("run","SP2",expname,pwrname,"ensemble$(mstr).sh")
+
+            open(nrun,"w") do wrun
+                sn = replace(s ,"[email]"       => )
+                sn = replace(sn,"[directory]"   => projectdir())
+                sn = replace(sn,"[experiment]"  => expname)
+                sn = replace(sn,"[config]"      => pwrname)
+                if ensembleii < 6
+                    sn = replace(sn,"[sndname]" => "$(expname)")
+                elseif (ensembleii > 5) && (ensembleii < 11)
+                    sn = replace(sn,"[sndname]" => "$(expname)_hot")
+                else
+                    sn = replace(sn,"[sndname]" => "$(expname)_cld")
+                end
+                sn = replace(sn,"[lsfname]"     => "noforcing")
+                sn = replace(sn,"[schname]"     => "SP2")
+                sn = replace(sn,"member[xx]"    => "member$(mstr)")
+                write(wrun,sn)
+            end
+
+        end
+
+    end
+end
+
+open(brun,"r") do frun
+    s = read(frun,String)
+    for pwrii in pwrvec
+
+        pwrname = relaxscalestrprnt(pwrii)
+        nrun = projectdir("run","SP2",expname,pwrname,"Build.csh")
+
+        open(nrun,"w") do wrun
+            sn = replace(s ,"[datadir]" => datadir())
+            sn = replace(sn,"[schname]" => "SP2")
+            sn = replace(sn,"[expname]" => expname)
+            sn = replace(sn,"[runname]" => pwrname)
+            write(wrun,sn)
+        end
+
+    end
+end
