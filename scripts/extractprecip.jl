7530e28a32d0f913e9fb5f37b6898ddcef72e144
Nathanael Wong <natgeo.wong@outlook.com>
2023-07-26 16:43:01 -0400

    Debug extraction scripts

--- scripts/extractprecip.jl
+++ scripts/extractprecip.jl
@@ -10,8 +10,8 @@ expname = "P1282km300V64"
 DGW1 = [0.2,0.5,1,2,5,10,20,50,100,200,500]
 DGW2 = [0.02,0.05,0.1,0.2,0.5,1,2,5,10,20,50,100,200,500]
 TGR  = [sqrt(2),2,2*sqrt(2.5),5,5*sqrt(2)]
-TGR1 = vcat(TGR1,10,TGR1*10)
-TGR2 = vcat(TGR1/10,1,TGR1,10,TGR1*10)
+TGR1 = vcat(TGR,10,TGR*10)
+TGR2 = vcat(TGR/10,1,TGR,10,TGR*10)
 
 pwrvec  = DGW1
 
